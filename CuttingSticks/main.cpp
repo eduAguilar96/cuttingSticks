3366477c36552fcf1196bc1430e8d7acacac9b80
Zareek Galván <zareek99@gmail.com>
2015-10-14 12:32:24 -0500

    First commit

---
+++ CuttingSticks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CuttingSticks
+//
+//  Created by Zareek Galvan on 10/13/15.
+//  Copyright © 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
