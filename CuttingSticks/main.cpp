99f103094e0ae41bee06931f3e2c7c80849e6d5d
Zareek Galván <zareek99@gmail.com>
2015-10-14 18:44:31 -0500

    fully functional in tutoring 24/7 but all in main method

--- CuttingSticks/main.cpp
+++ CuttingSticks/main.cpp
@@ -10,65 +10,58 @@
 #include <climits>
 using namespace std;
 
-int N = 52;
-int matriz[52][52];
-int arr[52];
-
-void llenaMatriz(int n)
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            matriz[i][j] = INT_MAX;
-        }
-    }
-    
-    for (int i = 0; i <= n; i++)
-    {
-        matriz[i][i] = 0;
-        matriz[i][i+1] = 0;
-        matriz[i][i+2] = arr[i+2] - arr[i];
-    }
-}
-
-void minimoCosto(int n)
-{
-    llenaMatriz(n);
-    
-    for (int k = 3; k<=n; k++)
-    {
-        for (int i = 0; i<=n; i++)
-        {
-            for (int j = i+1; j<=i+k-1; j++)
-            {
-                if ((matriz[i][j] + matriz[j][i+k] + arr[i+k] - arr[i]) < matriz[i][i+k])
-                {
-                    matriz[i][i+k] = matriz[i][j] + matriz[j][i+k] + arr[i+k] - arr[i];
-                }
-            }
-        }
-    }
-}
-
 int main()
 {
     int length;
     int casos;
+    int matriz[52][52];
+    int arr[52];
+    int N = 50;
     
-    while ((cin >> length) && (length != 0))
+    cin >> length;
+    
+    while (length != 0)
     {
         cin >> casos;
         
+        arr[0] = 0;
         for(int i = 0; i < casos; i++)
         {
             cin >> arr[i + 1];
         }
-        arr[0] = 0;
         casos++;
         arr[casos] = length;
-        minimoCosto(casos);
-        cout << "The minimum cutting is "<< matriz[0][casos] << "." << endl;
         
+        for (int i = 0; i <= N + 1; i++)
+        {
+            for (int j = 0; j <= N + 1; j++)
+            {
+                matriz[i][j] = INT_MAX;
+            }
+        }
+        
+        for (int i = 0; i <= casos; i++)
+        {
+            matriz[i][i] = 0;
+            matriz[i][i+1] = 0;
+            matriz[i][i+2] = arr[i+2] - arr[i];
+        }
+        
+        for (int k = 3; k<=casos; k++)
+        {
+            for (int i = 0; i<=casos-k; i++)
+            {
+                for (int j = i+1; j<=i+k-1; j++)
+                {
+                    if ((matriz[i][j] + matriz[j][i+k] + arr[i+k] - arr[i]) < matriz[i][i+k])
+                    {
+                        matriz[i][i+k] = matriz[i][j] + matriz[j][i+k] + arr[i+k] - arr[i];
+                    }
+                }
+            }
+        }
+        
+        cout << "The minimum cutting is "<< matriz[0][casos] << "." << endl;
+        cin >> length;
     }
 }
